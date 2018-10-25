08ba3eb678a039adf98b2448ede83ad5a7d778fe
AnthonyMikh <anthony.mikh@yandex.ru>
2018-10-25 22:10:24 +0300

    Update common.py

--- src/common/common.py
+++ src/common/common.py
@@ -55,31 +55,10 @@ def setup_fig(rect: Rect, size: int,             *, xlabel: str = 'x', ylabel: s
     plt.xlim(*rect.span_x)
     plt.ylim(*rect.span_y)
 
-def hypot(x0, y0, x1, y1):
-    return (x1 - x0)**2 + (y0 - y1)**2
+stream_params = {'density': 2, 'linewidth': 1, 'arrowsize': 2, 'arrowstyle': '->'}
+scatter_params = {'color': '#CD2305', 's': 80, 'marker': 'o', 'linewidth': 0}
 
-
-class Source:
-    def __init__(self, x: float, y: float, strength: float):
-        self.x = x
-        self.y = y
-        self.strength = strength
-    
-    @property
-    def coords(self) -> Coords:
-        return Coords(self.x, self.y)
-    
-    def __str__(self) -> str:
-        return f'Source: ({self.x}, {self.y}), strength = {self.strength}'
-    
-    def velocity(self, grid: Grid) -> Grid:
-        x, y = self.coords
-        X, Y = grid
-        coeff = self.strength / (2.0 * math.pi * hypot(x, y, X, Y))
-        u = coeff * (X - x)
-        v = coeff * (Y - y)
-        return (u, v)
-    
-    def potential(self, grid: Grid) -> 'numpy.ndarray':
-        X, Y = grid
-        return self.strength / (2.0 * math.pi) * np.arctan2(Y - self.y, X - self.x)
+def scatter_with(*args, override):
+    params = scatter_params.copy()
+    params.update(override)
+    plt.scatter(*args, **params)

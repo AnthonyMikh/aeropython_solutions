75de85a1218eb52177b1052bc4e39dc7e5c12a73
AnthonyMikh <anthony.mikh@yandex.ru>
2018-11-30 14:11:08 +0300

    Update zhukovsky.py

--- src/zhukovsky/zhukovsky.py
+++ src/zhukovsky/zhukovsky.py
@@ -1,2 +1,16 @@
-def zhukovsky(z: complex, c: complex) -> complex:
-    return z + (c*c / z)
+from typing import Callable
+
+def zhukovsky(c: complex) -> Callable[[complex], complex]:
+    def inner(z: complex) -> complex:
+        return z + (c*c / z)
+    return inner
+
+def zhukovsky_real(c: complex) -> Callable[[complex], complex]:
+    def inner(z: complex) -> complex:
+        return (z + (c*c / z)).real
+    return inner
+
+def zhukovsky_imag(c: complex) -> Callable[[complex], complex]:
+    def inner(z: complex) -> complex:
+        return (z + (c*c / z)).imag
+    return inner
